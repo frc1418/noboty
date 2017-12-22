2718e342ac48d9661e07e6fa469c7462964be6c6
ErikBoesen <me@erikboesen.com>
2017-12-21 21:11:15 -0500

    Basic code for drivability

---
+++ robot/components/drive.py
@@ -0,0 +1,41 @@
+import wpilib
+
+from networktables import NetworkTable
+from networktables.util import ntproperty
+
+ENCODER_ROTATION = 1023
+WHEEL_DIAMETER = 7.639
+
+class Drive:
+    robot_drive = wpilib.RobotDrive
+    sd = NetworkTable
+
+    def __init__(self):
+        self.sd = NetworkTable.getTable('/SmartDashboard')
+
+        self.enabled = False
+
+    def on_enable(self):
+        self.y = 0
+        self.rotation = 0
+
+    # Verb functions -- these functions do NOT talk to motors directly. This
+    # allows multiple callers in the loop to call our functions without
+    # conflicts.
+
+    def move(self, y, rotation, square_inputs=False):
+        """
+        Causes the robot to move
+        :param y: The speed that the robot should drive in the Y direction. -1 is forward. [-1.0..1.0]
+        :param rotation: The rate of rotation for the robot that is completely independent of the translation. 1 is rotate to the right [-1.0..1.0]
+        """
+        self.y = max(min(y, 1), -1)
+        self.rotation = max(min(1.0, rotation), -1)
+
+    def execute(self):
+        """Actually drive."""
+        self.robot_drive.arcadeDrive(self.y, -self.rotation, square_inputs=False)
+
+        # Prevent robot from driving by default
+        self.y = 0
+        self.rotation = 0
---
+++ robot/robot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import magicbot
+import wpilib
+
+from networktables.util import ntproperty
+from components import drive
+
+from robotpy_ext.common_drivers import navx
+
+from networktables.networktable import NetworkTable
+
+class Bot(magicbot.MagicRobot):
+    drive = drive.Drive
+
+    def createObjects(self):
+        # NavX (purple board on top of the RoboRIO)
+        self.navx = navx.AHRS.create_spi()
+
+        # Initialize SmartDashboard
+        self.sd = NetworkTable.getTable('SmartDashboard')
+
+        # Joysticks
+        self.left_joystick = wpilib.Joystick(0)
+        self.right_joystick = wpilib.Joystick(1)
+
+        self.lf_motor = wpilib.Victor(0)
+        self.lr_motor = wpilib.Victor(1)
+        self.rf_motor = wpilib.Victor(2)
+        self.rr_motor = wpilib.Victor(3)
+        self.robot_drive = wpilib.RobotDrive(self.lf_motor, self.lr_motor, self.rf_motor, self.rr_motor)
+
+    def autonomous(self): pass
+    def disabledPeriodic(self): pass
+    def disabledInit(self): pass
+    def teleopInit(self): pass
+
+    def teleopPeriodic(self):
+        self.drive.move(-self.left_joystick.getY(), self.right_joystick.getX())
+
+
+if __name__ == '__main__':
+    wpilib.run(Bot)
