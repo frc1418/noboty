bd68c0da8cc25fd4511713a3a788594801ac9e43
ErikBoesen <me@erikboesen.com>
2018-02-15 16:36:45 -0500

    Implement Intake component

---
+++ robot/components/intake.py
@@ -0,0 +1,39 @@
+import wpilib
+from magicbot import will_reset_to
+
+
+class Intake:
+    """
+    Operate robot intake.
+    """
+    intake_wheels: wpilib.SpeedControllerGroup
+
+    def __init__(self):
+        self._intake_wheel_speed = will_reset_to(0)
+
+    def spin(self, speed: float=1):
+        """
+        Set the speed of intake wheels.
+
+        :param speed: The requested speed, between -1 and 1.
+        """
+        self._intake_wheel_speed = speed
+
+    def pull(self):
+        """
+        Pull cube into intake.
+        """
+        self.spin(-1)
+
+    def push(self):
+        """
+        Push cube out of intake.
+        """
+        self.spin(1)
+
+    def execute(self):
+        """
+        Run intake motors.
+        """
+        self.shoulders.set(self._shoulder_speed)
+        self.intake_wheels.set(self._intake_wheel_speed)
