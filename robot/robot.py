43b07002ea9db7dcaf073e993a0a1428587ffe46
ErikBoesen <me@erikboesen.com>
2018-10-10 15:05:53 -0400

    Switch PWM ports and joystick configuration for new kitbot

--- robot/robot.py
+++ robot/robot.py
@@ -23,10 +23,14 @@ class Bot(magicbot.MagicRobot):
         #   Dig | 0/1
         #   2^1 | Left/Right
         #   2^0 | Front/Rear
-        self.lf_motor = wpilib.Victor(0b00)  # => 0
-        self.lr_motor = wpilib.Victor(0b01)  # => 1
-        self.rf_motor = wpilib.Victor(0b10)  # => 2
-        self.rr_motor = wpilib.Victor(0b11)  # => 3
+        #self.lf_motor = wpilib.Victor(0b00)  # => 0
+        #self.lr_motor = wpilib.Victor(0b01)  # => 1
+        #self.rf_motor = wpilib.Victor(0b10)  # => 2
+        #self.rr_motor = wpilib.Victor(0b11)  # => 3
+        self.lf_motor = wpilib.Victor(9)
+        self.lr_motor = wpilib.Victor(8)
+        self.rf_motor = wpilib.Victor(7)
+        self.rr_motor = wpilib.Victor(6)
 
         self.drivetrain = wpilib.drive.DifferentialDrive(wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor),
                                                     wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor))
@@ -53,12 +57,12 @@ class Bot(magicbot.MagicRobot):
 
     def teleopPeriodic(self):
         # Normal joysticks
-        #self.drive.move(-self.joystick.getY(),self.joystick.getX())
+        self.drive.move(-self.joystick.getY(),self.joystick.getX())
 
         # Corrections for aviator joystick
-        self.drive.move(-2*(self.joystick.getY()+.5),
-                        2*(self.joystick.getX()+.5)+ROT_COR,
-                        sarah=self.sarah)
+        #self.drive.move(-2*(self.joystick.getY()+.5),
+        #                2*(self.joystick.getX()+.5)+ROT_COR,
+        #                sarah=self.sarah)
 
         if self.btn_sarah:
             self.sarah = not self.sarah
